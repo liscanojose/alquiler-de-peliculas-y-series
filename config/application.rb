6a8657b8ff4e8ae06ec93e9815e6c03f15cc29e4
Jose <jose87javier@gmail.com>
2017-12-12 19:51:11 -0300

    agregadas traducciones y mejoras en paginacion

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,13 @@ module Alquiler
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.encoding = "utf-8"
+    config.time_zone = 'America/Santiago'
+    #i18n
+    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :es
+    config.i18n.locale = :es
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.enabled = true
     config.assets.paths << Rails.root.join("app", "assets", "docsupport")
